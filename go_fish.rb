01572c04fa17110d1ea95ead811e5eb0c8ed4951
Anne Jhetson <annepsu@hotmail.com>
2014-03-15 04:12:09 +0000

    finalize ui for 2 player functionality

--- go_fish.rb
+++ go_fish.rb
@@ -7,22 +7,23 @@ def new_game
   player_1 = Player.new(gets.chomp.capitalize)
   puts "Enter Player 2 Name"
   player_2 = Player.new(gets.chomp.capitalize)
-  
-  start_game(0, player_1, player_2)
+  turn = 1
+  turn_player = ""
+  non_turn_player = ""
+  start_game(turn, player_1, player_2)
 end
 
 def start_game(turn, player_1, player_2)
-  turn_player = ""
-  non_turn_player = ""
-      if turn % 2 == 1
+  if turn.odd?
         turn_player = player_1
         non_turn_player = player_2
-      else
+  else     
         turn_player = player_2
         non_turn_player = player_1
-      end
+  end
+  puts "****Turn Player:  #{turn_player.name} ****"
+  puts "****Non Turn Player #{non_turn_player.name} ****"
   start_length = turn_player.hand.length
-  
   puts "\n#{turn_player.name} Cards:\n"
   puts turn_player.hand
   puts "---------------------------------------------------\n"
@@ -32,41 +33,28 @@ def start_game(turn, player_1, player_2)
   puts "\n#{turn_player.name}, what card number do you want?"
 
   choice = gets.chomp
+  @temp_array = []
   non_turn_player.hand.each do |card|
-      if card[0..1].include?(choice)
-        p card
-      @temp_array = []
+    if card[0..1].include?(choice)
       @temp_array << card
-        p @temp_array
-     end
+    end
   end
+  
   print @temp_array
   turn_player.hand.concat(@temp_array)
-  non_turn_player.hand = non_turn_player.hand - @temp_array
+  @temp_array.each do |card|
+    non_turn_player.hand.reject! { |hand| hand == card}
+  end
   
-  puts "\n\n#{turn_player.name} Cards:\n"
-  puts turn_player.hand
-  puts "---------------------------------------------------\n"
-  puts puts "\n\n#{non_turn_player.name}  Cards:\n"
-  puts non_turn_player.hand
-  puts "---------------------------------------------------\n"
-
   if turn_player.hand.length == start_length
-    puts "Go fish!"
-    go_fish(turn, turn_player, non_turn_player)
+    puts "Go fish!¸.·´¯`·.´¯`·.¸¸.·´¯`·.¸><(((º>\n"
+    puts "1 card added to #{turn_player.name}\n\n"
+    turn_player.hand << Player.cards.slice!(0, 1)
+    #go_fish(turn, turn_player, non_turn_player)
     turn += 1
+    start_game(turn, player_1, player_2)    
   end
   start_game(turn, player_1, player_2)
-end
-
-def go_fish(turn, turn_player, non_turn_player)
-  turn_player.hand << Player.cards.slice!(0, 1)
-  puts "\n#{turn_player.name} Cards:\n"
-  puts turn_player.hand
-  puts "---------------------------------------------------\n"
-  puts Player.cards.length
-  turn += 1
-  start_game(turn, turn_player, non_turn_player)
 end
 
 new_game
